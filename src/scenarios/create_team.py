b57ee0cbf1662f0e0e24fbdf5b6434668aab553d
Anton Zimin <56966172+zimkaa@users.noreply.github.com>
2024-03-30 17:59:30 +0500

    fix: bug
    Signed-off-by: Anton Zimin <56966172+zimkaa@users.noreply.github.com>

--- src/scenarios/create_team.py
+++ src/scenarios/create_team.py
@@ -8,5 +8,5 @@ def create_teams(players: list[User]) -> tuple[Team, Team]:
     shuffle(players)
     middle_index = len(players) // 2
     team1 = players[0:middle_index]
-    players[middle_index:]
-    return (Team(players=team1), Team(players=team1))
+    team2 = players[middle_index:]
+    return (Team(players=team1), Team(players=team2))
